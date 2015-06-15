d23485f5b47abbd7b677e1047b1cf3209c90cd62
admin <admin@Jamess-MacBook-Pro.local>
2015-06-14 17:57:41 -0700

    basic auth

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,19 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  //auth routes
+
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback',
 
   /***************************************************************************
   *                                                                          *
